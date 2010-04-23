f64704bbf2910fadd3a990d3bcc9ccc5e1be8197
sergiokozlov <sn.kozlov@yahoo.com>
2010-04-23 17:58:18 +0400

    adding date helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,5 +21,17 @@ module ApplicationHelper
   def to_time(minutes)
     leading_zero(h60(minutes)) + ':' + leading_zero(m60(minutes))
   end
-  
+ 
+  #date helpers
+  def jan1
+    Date.parse("#{today.year}-01-01")
+  end
+
+  def week_last_day(week)
+    jan1 + 7*week + 7 - jan1.cwday
+  end 
+
+  def week_first_day(week)
+    jan1 + 7*week - jan1.cwday
+  end
 end
