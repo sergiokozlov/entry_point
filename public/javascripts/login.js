c3ba6bd231bb6d11611077654499ce4994de4d41
ivanbabko <ivan.babko@gmail.com>
2010-10-19 20:06:36 +0400

    finalized login look

---
+++ public/javascripts/login.js
@@ -0,0 +1,19 @@
+$(function () {
+	
+	// Focus on first visible input field
+	$(window).load(function () {
+		$(':input:visible:enabled:first').focus();
+	});
+	
+	// Login error
+	//$('#user_session_submit').click( function() {
+	//	$('.error').slideDown('fast').delay(3000).slideUp();
+	//	return false;
+	//});
+	  
+})
+
+
+
+
+
