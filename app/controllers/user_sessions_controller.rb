f3002e7155b5e6fe88f24d92a1586d2c61ae98fd
sergiokozlov <sn.kozlov@yahoo.com>
2010-10-20 16:55:14 +0400

    removing flash call

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -14,7 +14,7 @@ class UserSessionsController < ApplicationController
   def create  
     @user_session = UserSession.new(params[:user_session])  
     if @user_session.save  
-     #flash[:notice] = "Successfully logged in."
+     flash[:notice] = "Successfully logged in."
       if current_user.manager?  
         redirect_to :controller => 'dashboard', :action => 'manage'
       else
