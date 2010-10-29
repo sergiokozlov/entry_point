ec0a298248e9ab71f813784ff6697db21b52b7c1
ivanbabko <ivan.babko@gmail.com>
2010-10-29 14:51:53 +0400

    Updated login error message behavior to be more user friendly

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,31 +1,34 @@
-jQuery.ajaxSetup({  
+$.ajaxSetup({  
     'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}  
 });
 
 $(function () {
 
-    // Focus on first visible input field
-    $(window).load(function () {
-      $(':input:visible:enabled:first').focus();
 
-      $('#new_user_session').submit(function (){  
-        $.post($(this).attr('action'), $(this).serialize(), function(msg){
-          if (msg == 'Bad') {
-          //alert( "Data Saved: " + msg );
-          $('.alert').slideDown('fast'); }
-          });  
-        return false;
-        });
+	// Focus on first visible input field
+	$(':input:visible:enabled:first').focus();
+	
+	
+	// Make AJAX call to users_controller
+	$('#new_user_session').submit(function (){  
+		$.post($(this).attr('action'), $(this).serialize(), function(msg){
+			if (msg == 'Bad') {
+				if ( $('.alert').css('display')=='block' ) {
+					$('.alert').fadeOut('fast').fadeIn('fast').fadeOut('fast').fadeIn('fast'); 
+				} else {
+					$('.alert').slideDown('fast');
+				}
+			} else {
+				if ( $('.alert').css('display')=='block' ) {
+					$('.alert').slideUp('fast'); 
+				}
+			}
+		});  
+		return false;
+	});
+	
 
-      });
-
-    // Login error
-    //$('#user_session_submit').click( function() {
-    //	$('.error').slideDown('fast').delay(3000).slideUp();
-	//	return false;
-	//});
-	  
-})
+});
 
 
 
