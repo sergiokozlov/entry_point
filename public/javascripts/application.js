1341b697db653c6270812db48b08ade2369aa507
ivanbabko <ivan.babko@gmail.com>
2010-11-08 02:32:46 +0300

    * Reverted table headers to dark color. * Added modal dialog placeholder

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -162,6 +162,50 @@ $(document).ready( function () {
 	//Zebra tables
 	$('table tbody tr:odd').addClass('alt')
 	
+	
+	
+	//**************** START MODAL DIALOG *******************//
+	
+	//When you click on a link with class of dialog-trigger and the href starts with a # 
+	$('a.dialog-trigger[href^=#]').click(function() {
+		var popID = $(this).attr('rel'); //Get Popup Name
+		var popURL = $(this).attr('href'); //Get Popup href to define size
+
+		//Pull Query & Variables from href URL
+		var query= popURL.split('?');
+		var dim= query[1].split('&');
+		var popWidth = dim[0].split('=')[1]; //Gets the first query string value
+
+		//Fade in the Popup
+		$('#' + popID).fadeIn().css({ 'width': Number( popWidth ) });
+
+		//Define margin for center alignment (vertical   horizontal)
+		var popMargTop = ($('#' + popID).height() ) / 2;
+		var popMargLeft = ($('#' + popID).width() ) / 2;
+
+		//Apply Margin to Popup
+		$('#' + popID).css({
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+
+		//Show Background
+		$('body').append('<div id="fade"></div>'); //Add the fade layer to bottom of the body tag.
+		$('#fade').css({'filter' : 'alpha(opacity=40)'}).show(); //Fade in the fade layer - .css({'filter' : 'alpha(opacity=80)'}) is used to fix the IE Bug on fading transparencies 
+
+		return false;
+	});
+
+	//Close Popups and Fade Layer
+	$('a.close, #fade').live('click', function() { //When clicking on the close or fade layer...
+		$('#fade , .dialog').fadeOut(function() {
+			//$('#fade, a.close').remove();  //fade them both out
+		});
+		return false;
+	});
+	
+	//**************** END MODAL DIALOG *******************//
+	
 }); 
 
 
