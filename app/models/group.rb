568a923ae7fb9f14c7eaf2d060a26970b104d63c
Sergio Kozlov <sn.kozlov@yahoo.com>
2012-01-23 13:26:30 +0400

    correcting weeks and months ordering in dropdown

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,7 +8,7 @@ class Group < ActiveRecord::Base
     self.developers.each do |dev| 
       result += dev.logged_working_weeks
     end
-    result.uniq.sort {|a,b| b<=> a}
+    result.uniq.sort {|a,b| b.reverse <=> a.reverse}
   end
 
   def months_to_analyze
@@ -17,7 +17,7 @@ class Group < ActiveRecord::Base
     self.developers.each do |dev| 
       result += dev.logged_working_months
     end
-    result.uniq.sort {|a,b| b<=> a}
+    result.uniq.sort {|a,b| b.reverse <=> a.reverse}
   end
 
   def alerts
